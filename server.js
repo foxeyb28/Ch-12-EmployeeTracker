3dbb83e2dbc777995b920703f923296e32557bc6
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-09-14 23:02:34 -0500

    added folders and files

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ const db = mysql.createConnection(
   );
 
   db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
-    console.log(results);
-  });
-  
-  // Query database using SUM(), MAX(), MIN() AVG() and GROUP BY
-  db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
-    console.log(results);
-  });
+  console.log(results);
+});
+
+// Query database using SUM(), MAX(), MIN() AVG() and GROUP BY
+db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
+  console.log(results);
+});
 
   app.use((req, res) => {
     res.status(404).end();
