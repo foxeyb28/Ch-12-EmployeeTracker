29c27e2124bd0ba667468a2b8aa295a3febdef65
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-09-18 17:51:13 -0500

    added more coding

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (name) VALUES 
+("name 1"),
+("name 2"),
+("name 3");
+INSERT INTO role (title, salary, depid) VALUES 
+("title 1", 90000, 1),
+("title 2", 90000, 2),
+("title 3", 90000, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
+("jane", "doe", 1, null),
+("jane", "doe", 2, null),
+("jane", "doe", 3, null);
--- server.js
+++ server.js
@@ -1,34 +1,146 @@
-const express = require('express');
 const mysql = require('mysql2');
-const PORT = process.env.PORT || 3001;
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const inquirer = require("inquirer")
 const db = mysql.createConnection(
     {
       host: 'localhost',
       user: 'root',
-      password: '',
-      database: 'books_db'
+      password: 'abc1234',
+      database: 'employees_db'
     },
     console.log(`Connected to the books_db database.`)
   );
 
-  db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
-  console.log(results);
-});
+  function start(){
+    inquirer.prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "start",
+        choices: ["View Departments", "View Roles", "View Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role"],
+      }
+    ])
+    .then(answer => {
+      console.log(answer)
+      if(answer.start == "View Departments"){
+        viewDepartments()
+      }
+      if(answer.start == "View Roles"){
+        viewRoles()
+      }
+      if(answer.start == "View Employees"){
+        viewEmployees()
+      }
+      if(answer.start == "Add Department"){
+        addDepartment()
+      }
+      if(answer.start == "Add Role"){
+        addRole()
+      }
+      if(answer.start == "Add Employee"){
+        addEmployee()
+      }
+      if(answer.start == "Update Employee Role"){
+        updateEmployeeRole()
+      }
+  })
 
-// Query database using SUM(), MAX(), MIN() AVG() and GROUP BY
-db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
-  console.log(results);
-});
+  }
 
-  app.use((req, res) => {
-    res.status(404).end();
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-  
-  
+  function viewDepartments(){
+    db.query("select * from department", (err, res) =>{
+      if (err) console.log(err)
+      console.table(res)
+    start()
+    })
+  }
+  function viewRoles(){
+    db.query("select * from role", (err, res) =>{
+      if (err) console.log(err)
+      console.table(res)
+    start()
+    })
+  }
+  function viewEmployees(){
+    db.query("select * from employee", (err, res) =>{
+      if (err) console.log(err)
+      console.table(res)
+    start()
+    })
+  }
+  function addDepartment(){
+    inquirer.prompt({
+      type: "input",
+      name: "dep",
+      message: "What is the department name?"
+    })
+    .then(answer => {
+      db.query(`insert into department (name) values ("${answer.dep}")`, (err, res) =>{
+        if (err) console.log(err)
+        console.table(res)
+      start()
+      })
+    })
+  }
+  function addRole(){
+    inquirer.prompt([
+      {
+      type: "input",
+      name: "title",
+      message: "What is the title?"
+    },
+      {
+      type: "input",
+      name: "salary",
+      message: "What is the salary?"
+    },
+      {
+      type: "input",
+      name: "depid",
+      message: "What is the department id?"
+    },
+  ])
+    .then(answers => {
+      db.query(`insert into role (title, salary, depid) values ("${answers.title}", ${answers.salary}, ${answers.depid})`, (err, res) =>{
+        if (err) console.log(err)
+        console.table(res)
+      start()
+      })
+    })
+  }
+  function addEmployee(){
+    inquirer.prompt([
+      {
+      type: "input",
+      name: "firstname",
+      message: "What is the first name?"
+    },
+      {
+      type: "input",
+      name: "lastname",
+      message: "What is the last name?"
+    },
+      {
+      type: "input",
+      name: "roleid",
+      message: "What is the role id?",
+
+    },
+      {
+      type: "input",
+      name: "managerid",
+      message: "What is the manager id?"
+    },
+  ])
+    .then(answers => {
+      
+      db.query(`insert into employee (first_name, last_name, role_id, manager_id) values ("${answers.firstname}", "${answers.lastname}", ${answers.roleid}, null)`, (err, res) =>{
+        if (err) console.log(err)
+        console.table(res)
+      start()
+      })
+    })
+  }
+  function updateEmployeeRole(){
+
+  }
+  start()
