fd7aebbcc199cc83640bcbf10f1f134dac991e57
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-09-18 23:27:56 -0500

    added to readme and serverjs

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ AS A business owner
 I WANT to be able to view and manage the departments, roles, and employees in my company
 SO THAT I can organize and plan my business
 
+I used mysql to create my database and run the program with node server
+
+I was challeneged with my update employee last name came back undefined and I wasn't able to fix it.
+
+
+https://drive.google.com/file/d/1IPGi1mLQEVrup-8bSu3IVHHAozk2kUfA/view
+
--- server.js
+++ server.js
@@ -1,146 +1,196 @@
-const mysql = require('mysql2');
-const inquirer = require("inquirer")
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: 'abc1234',
-      database: 'employees_db'
-    },
-    console.log(`Connected to the books_db database.`)
-  );
+  {
+    host: "localhost",
+    user: "root",
+    password: "abc1234",
+    database: "employees_db",
+  },
+  console.log(`Connected to the books_db database.`)
+);
 
-  function start(){
-    inquirer.prompt([
+function start() {
+  inquirer
+    .prompt([
       {
         type: "list",
         message: "What would you like to do?",
         name: "start",
-        choices: ["View Departments", "View Roles", "View Employees", "Add Department", "Add Role", "Add Employee", "Update Employee Role"],
-      }
+        choices: [
+          "View Departments",
+          "View Roles",
+          "View Employees",
+          "Add Department",
+          "Add Role",
+          "Add Employee",
+          "Update Employee Role",
+        ],
+      },
     ])
-    .then(answer => {
-      console.log(answer)
-      if(answer.start == "View Departments"){
-        viewDepartments()
+    .then((answer) => {
+      console.log(answer);
+      if (answer.start == "View Departments") {
+        viewDepartments();
       }
-      if(answer.start == "View Roles"){
-        viewRoles()
+      if (answer.start == "View Roles") {
+        viewRoles();
       }
-      if(answer.start == "View Employees"){
-        viewEmployees()
+      if (answer.start == "View Employees") {
+        viewEmployees();
       }
-      if(answer.start == "Add Department"){
-        addDepartment()
+      if (answer.start == "Add Department") {
+        addDepartment();
       }
-      if(answer.start == "Add Role"){
-        addRole()
+      if (answer.start == "Add Role") {
+        addRole();
       }
-      if(answer.start == "Add Employee"){
-        addEmployee()
+      if (answer.start == "Add Employee") {
+        addEmployee();
       }
-      if(answer.start == "Update Employee Role"){
-        updateEmployeeRole()
+      if (answer.start == "Update Employee Role") {
+        updateEmployeeRole();
       }
-  })
+    });
+}
 
-  }
-
-  function viewDepartments(){
-    db.query("select * from department", (err, res) =>{
-      if (err) console.log(err)
-      console.table(res)
-    start()
-    })
-  }
-  function viewRoles(){
-    db.query("select * from role", (err, res) =>{
-      if (err) console.log(err)
-      console.table(res)
-    start()
-    })
-  }
-  function viewEmployees(){
-    db.query("select * from employee", (err, res) =>{
-      if (err) console.log(err)
-      console.table(res)
-    start()
-    })
-  }
-  function addDepartment(){
-    inquirer.prompt({
+function viewDepartments() {
+  db.query("select * from department", (err, res) => {
+    if (err) console.log(err);
+    console.table(res);
+    start();
+  });
+}
+function viewRoles() {
+  db.query("select * from role", (err, res) => {
+    if (err) console.log(err);
+    console.table(res);
+    start();
+  });
+}
+function viewEmployees() {
+  db.query("select * from employee", (err, res) => {
+    if (err) console.log(err);
+    console.table(res);
+    start();
+  });
+}
+function addDepartment() {
+  inquirer
+    .prompt({
       type: "input",
       name: "dep",
-      message: "What is the department name?"
+      message: "What is the department name?",
     })
-    .then(answer => {
-      db.query(`insert into department (name) values ("${answer.dep}")`, (err, res) =>{
-        if (err) console.log(err)
-        console.table(res)
-      start()
-      })
-    })
-  }
-  function addRole(){
-    inquirer.prompt([
+    .then((answer) => {
+      db.query(
+        `insert into department (name) values ("${answer.dep}")`,
+        (err, res) => {
+          if (err) console.log(err);
+          console.table(res);
+          start();
+        }
+      );
+    });
+}
+function addRole() {
+  inquirer
+    .prompt([
       {
-      type: "input",
-      name: "title",
-      message: "What is the title?"
-    },
+        type: "input",
+        name: "title",
+        message: "What is the title?",
+      },
       {
-      type: "input",
-      name: "salary",
-      message: "What is the salary?"
-    },
+        type: "input",
+        name: "salary",
+        message: "What is the salary?",
+      },
       {
-      type: "input",
-      name: "depid",
-      message: "What is the department id?"
-    },
-  ])
-    .then(answers => {
-      db.query(`insert into role (title, salary, depid) values ("${answers.title}", ${answers.salary}, ${answers.depid})`, (err, res) =>{
-        if (err) console.log(err)
-        console.table(res)
-      start()
-      })
-    })
-  }
-  function addEmployee(){
-    inquirer.prompt([
+        type: "input",
+        name: "depid",
+        message: "What is the department id?",
+      },
+    ])
+    .then((answers) => {
+      db.query(
+        `insert into role (title, salary, depid) values ("${answers.title}", ${answers.salary}, ${answers.depid})`,
+        (err, res) => {
+          if (err) console.log(err);
+          console.table(res);
+          start();
+        }
+      );
+    });
+}
+function addEmployee() {
+  inquirer
+    .prompt([
       {
-      type: "input",
-      name: "firstname",
-      message: "What is the first name?"
-    },
+        type: "input",
+        name: "firstname",
+        message: "What is the first name?",
+      },
       {
-      type: "input",
-      name: "lastname",
-      message: "What is the last name?"
-    },
+        type: "input",
+        name: "lastname",
+        message: "What is the last name?",
+      },
       {
-      type: "input",
-      name: "roleid",
-      message: "What is the role id?",
-
-    },
+        type: "input",
+        name: "roleid",
+        message: "What is the role id?",
+      },
       {
-      type: "input",
-      name: "managerid",
-      message: "What is the manager id?"
-    },
-  ])
-    .then(answers => {
-      
-      db.query(`insert into employee (first_name, last_name, role_id, manager_id) values ("${answers.firstname}", "${answers.lastname}", ${answers.roleid}, null)`, (err, res) =>{
-        if (err) console.log(err)
-        console.table(res)
-      start()
-      })
-    })
-  }
-  function updateEmployeeRole(){
-
-  }
-  start()
+        type: "input",
+        name: "managerid",
+        message: "What is the manager id?",
+      },
+    ])
+    .then((answers) => {
+      db.query(
+        `insert into employee (first_name, last_name, role_id, manager_id) values ("${answers.firstname}", "${answers.lastname}", ${answers.roleid}, null)`,
+        (err, res) => {
+          if (err) console.log(err);
+          console.table(res);
+          start();
+        }
+      );
+    });
+}
+function updateEmployeeRole() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstname",
+        message: "What is the first name?",
+      },
+      {
+        type: "input",
+        name: "lastname",
+        message: "What is the last name?",
+      },
+      {
+        type: "input",
+        name: "roleid",
+        message: "What is the roleid?",
+      },
+      {
+        type: "input",
+        name: "managerid",
+        message: "What is the manager id?",
+      },
+    ])
+    .then((answers) => {
+      db.query(
+        `update employee (first_name, last_name, role_id, manager_id) values ("${answers.firstname}", "${answers.lastname}", "${answers.roleid}", null)`,
+        (err, res) => {
+          if (err) console.log(err);
+          console.table(res);
+          start();
+        }
+      );
+    });
+}
+start();
